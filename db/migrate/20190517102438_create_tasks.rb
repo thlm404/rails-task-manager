3bf0ec78b0e46928b58e3c04e76c1eb9bbdb57ce
tom halimi <tomcompteperso@gmail.com>
2019-05-17 16:52:15 +0200

    Rails Day 2 exo 2

--- db/migrate/20190517102438_create_tasks.rb
+++ db/migrate/20190517102438_create_tasks.rb
@@ -2,10 +2,8 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :details
-      t.string :text
-      t.string :completed
-      t.string :boolean
+      t.text :details
+      t.boolean :completed, default: false
 
       t.timestamps
     end
